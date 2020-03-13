193ccaa5385216815a3c2737505f1712e0f8108d
Bob (SonAE) <bob@example.com>
2020-03-13 12:13:06 +0300

    code:search min

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b, MAX;
+
+    int a, b, max, min;
     cin >> a >> b;
 
     max = a + b;
@@ -18,12 +19,27 @@ int main()
     if (a/b>max)
         max=a/b;
 
+    min = a + b;
+    if (a-b<min)
+        min = a-b;
+
+    if (a*b<min)
+        min = a*b;
+
+    if (a/b<min)
+       min=a/b;
+
+
 
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n'
-         << "max = " << max << '\n';
+
+         << "max = " << max << '\n'
+
+         << "min = "<< min <<'\n';
+
 
 
     return 0;
