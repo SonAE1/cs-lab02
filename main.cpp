ec93d69e1eed88a7f7be203b36ec314f41d87194
Alice (SonAE) <alice@example.com>
2020-03-13 12:10:22 +0300

    code: search max

--- main.cpp
+++ main.cpp
@@ -5,13 +5,25 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    int a, b, MAX;
     cin >> a >> b;
+
+    max = a + b;
+    if (a-b>max)
+        max = a-b;
+
+    if (a*b>max)
+        max = a*b;
+
+    if (a/b>max)
+        max=a/b;
+
 
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
-         << "A / B = " << a / b << '\n';
+         << "A / B = " << a / b << '\n'
+         << "max = " << max << '\n';
 
 
     return 0;
